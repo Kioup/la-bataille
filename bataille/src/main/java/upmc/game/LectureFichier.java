f5122fac8b1bc611d5938fd2bf4d347abe590c58
Kioup <mftrookie@gmail.com>
2017-11-17 09:07:26 +0100

    Lecturefichier factorisé

--- bataille/src/main/java/upmc/game/LectureFichier.java
+++ bataille/src/main/java/upmc/game/LectureFichier.java
@@ -20,18 +20,11 @@ public class LectureFichier implements LecturePseudo {
 
     public ArrayList<String> lirePseudo() {
         
-        FileReader monFichier = null;
-        BufferedReader tampon = null;
-        System.out.println("############### Indiquer le nom du fichier sans l'extension : ##############");
-        System.out.println("Au cas ou vous ne souhaitez pas en créer un, il existe un fichier pseudo.txt qui contient des pseudonymes par defaut");
-        System.out.println("############## Pour l'utiliser, indiquer juste \"pseudo\" dans la console. ##############");
-        Scanner kl = new Scanner(System.in);
-        File test = new File("test.txt");
-        String path = test.getAbsolutePath().replace("test.txt","");
-        String NomFic = path+"..\\"+kl.nextLine()+".txt";
-        ArrayList AL = new ArrayList<String>();
+        this.LectureFicMessageDebut();
+        ArrayList ficPseudo = new ArrayList<String>();
         boolean verif = false;
-        
+        String path = this.CreerChemin();
+        String NomFic = this.CheminFic(path);
         String fichier = "";
         File Fichier = new File(NomFic);
         while (verif == false){   
@@ -39,37 +32,66 @@ public class LectureFichier implements LecturePseudo {
             Fichier = new File(NomFic);
             if (Fichier.exists()){
                 verif = true;
-                try {
-                    monFichier = new FileReader(NomFic);
-                        tampon = new BufferedReader(monFichier);
-                        while (true) {
-                                // Lit une ligne du fichier .txt
-                                String ligne = tampon.readLine();
-                                // Vérifie la fin de fichier
-                                if (ligne == null) {
-                                    break;
-                                }
-                                //Ajoute la ligne à la ligne.
-                                AL.add(ligne);
-                            } // Fin du while 
-                    } catch (IOException exception) {
-                        exception.printStackTrace();
-                    } finally {
-                        try {
-                            tampon.close();
-                            monFichier.close();
-                        } catch (IOException exception1) {
-                            exception1.printStackTrace();
-                        }              
-
-                        }  
-                    }
-                else {
-                    System.out.println("Le nom du fichier spécifié est incorrect ou n'existe pas !");
-                    System.out.println("############## Veuillez réessayer ##############");
-                    NomFic = path+"..\\"+kl.nextLine() + ".txt";
-                } 
+                ficPseudo = this.nomFic(NomFic, ficPseudo);
+            }
+            else {
+                this.LectureFicMessageFin();
+                NomFic = this.CheminFic(path);
+            } 
         }
+        return ficPseudo;
+    }
+    
+    public void LectureFicMessageDebut(){
+        System.out.println("############### Indiquer le nom du fichier sans l'extension : ##############");
+        System.out.println("Au cas ou vous ne souhaitez pas en créer un, il existe un fichier pseudo.txt qui contient des pseudonymes par defaut");
+        System.out.println("############## Pour l'utiliser, indiquer juste \"pseudo\" dans la console. ##############");
+    }
+    
+    public void LectureFicMessageFin() {
+        System.out.println("Le nom du fichier spécifié est incorrect ou n'existe pas !");
+        System.out.println("############## Veuillez réessayer ##############");
+    }
+    
+    public ArrayList<String> nomFic(String NomFic, ArrayList AL){
+    try {
+                FileReader monFichier = new FileReader(NomFic);
+                BufferedReader tampon = new BufferedReader(monFichier);
+                while (true) {
+                        // Lit une ligne du fichier .txt
+                        String ligne = tampon.readLine();
+                        // Vérifie la fin de fichier
+                        if (ligne == null) {
+                            break;
+                        }
+                        //Ajoute la ligne à la ligne.
+                        AL.add(ligne);
+                } // Fin du while 
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            } finally {
+                try {
+                    FileReader monFichier = new FileReader(NomFic);
+                    BufferedReader tampon = new BufferedReader(monFichier);
+                    tampon.close();
+                    monFichier.close();
+                } catch (IOException exception1) {
+                    exception1.printStackTrace();
+                }              
+
+            }  
         return AL;
+    }
+    
+    public String CheminFic(String path){
+        Scanner kl = new Scanner(System.in);
+        String NomFic = path+"..\\"+kl.nextLine()+".txt";
+        return NomFic;
+    }
+    
+    public String CreerChemin(){
+        File test = new File("test.txt");
+        String path = test.getAbsolutePath().replace("test.txt","");
+        return path;
     }
 }
